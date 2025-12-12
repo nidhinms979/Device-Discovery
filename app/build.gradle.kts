4c13b5740133ed12eaa92ba1440f8494f3e5caf6
nidhin <nidhin@revyrieglobal.com>
2025-12-12 20:25:34 +0530

    chore: initial project setup with dependencies - Add Room database dependencies - Add Google Play Services Auth - Add Material Design components - Add Kotlin coroutines support - Configure build settings and permissions in manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+    id("kotlin-kapt")
 }
 
 android {
@@ -42,7 +44,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.room.common.jvm)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    kapt("androidx.room:room-compiler:2.8.4")
+
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.android.gms:play-services-auth:21.4.0")
 }
